d954eca1e27085ab8dfcab576c81ee752cc48414
jooshhart <jooshhart@gmail.com>
2023-09-21 22:36:25 -0600

    y

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,4 +6,13 @@ class Program
     {
         Console.WriteLine("Hello Learning02 World!");
     }
+
+    public void Job()
+    {
+        Console.WriteLine("Software Engineer (Microsoft) 2019-2022");
+    }
+    public void Resume()
+    {
+        Console.WriteLine("Resume");
+    }
 }
