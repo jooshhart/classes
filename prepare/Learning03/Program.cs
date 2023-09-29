97abb3a350498b6ea18c62f114458469f940345c
jooshhart <jooshhart@gmail.com>
2023-09-28 22:03:11 -0600

    y

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,55 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Account savings = new Account();
+        savings._balance = 50;
+        savings.Deposit(100);  
+        Person p = new Person();
+        p.SetFirstName("Peter");
+
+        Console.WriteLine(p.GetFirstName());
+        Person p = new Person(); 
+        Person p1 = new Person(); 
+        Person p2 = new Person("Jane", "Doe"); 
+        Person p3 = new Person("Mrs.", "Jane", "Doe"); 
     }
+}
+
+public class Account
+{
+    private List<int> _transactions = new List<int>(); 
+
+    public void Deposit(int amount)
+    {
+        _transactions.Add(amount);
+    }
+}
+
+public class Person
+{
+    private string _title;
+    private string _firstName;
+    private string _lastName;
+
+    public Person()
+    {
+        _title = "";
+        _firstName = "Anonymous";
+        _lastName = "Unknown";
+    }
+
+    public Person(string first, string last)
+    {
+        _title = "";
+        _firstName = first;
+        _lastName = last;
+    }
+
+    public Person(string title, string first, string last)
+    {
+        _title = title;
+        _firstName = first;
+        _lastName = last;
+    }
+
 }
