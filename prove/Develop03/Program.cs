4fc74974be21da920aee2a216b2d81f83a5d9cac
jooshhart <jooshhart@gmail.com>
2023-10-03 22:31:53 -0600

    y

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string passage = "Helaman 12:7-8";
+        string scripture = "7 O how great is the anothingness of the children of men; yea, even they are bless than the dust of the earth.\n8 For behold, the dust of the earth moveth hither and thither, to the dividing asunder, at the command of our great and everlasting God.";
+        Console.WriteLine(passage);
+        Console.WriteLine(scripture);
+        string[] scriparray = scripture.Split(' ');
+        string piece = "";
+
     }
+}
+
+public class Input
+{
+
+}
+
+public class Rewrite
+{
+
+}
+
+public class Scrip
+{
+
+System.Random random = new System.Random();
+
+
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,12 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        string[] prompt = {"What would you do differently today?", "Name two things you're grateful for today:", "Anything interesting happen today?"};
         System.Random random = new System.Random();
-        string var = prompt[random.Next(0, prompt.Length)];
-        Console.WriteLine($"{var}");
-        string ans = Console.ReadLine();
-        string writ = $"{var} + {ans}";
-        Console.WriteLine(writ);
+        
+        int count = 0;
+        while(count!=20)
+        {
+            Console.WriteLine(Enumerable.Range(0,100).OrderBy(x => random.Next()).Take(20));
+            count+=1;
+        };
     }
 }
